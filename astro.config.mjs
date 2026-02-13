071acddd14892d1c62745b8e610ea32bcaa5326f
WL8202 <rbony@student.42lyon.fr>
2026-02-13 21:33:55 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://robinbony.github.io",
+  base: "/lacamwsh",
   vite: {
     plugins: [tailwindcss()],
   },
